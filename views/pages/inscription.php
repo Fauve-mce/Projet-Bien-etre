5593514516fa741ec19b7b84a79b882689431a00
Fauve-mce <cindy.mareels@outlook.be>
2023-09-20 12:03:46 +0200

    création contact + debut inscription

--- views/pages/inscription.php
+++ views/pages/inscription.php
@@ -1,6 +1,6 @@
 
 <main>
-<h1>Inscription<h/1>
+<h1>Connexion<h/1>
 
 <section class="flex flex-col md:flex-row h-screen items-center">
 
@@ -14,26 +14,26 @@
     <div class="w-full h-100">
 
 
-      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Connexion</h1>
 
       <form class="mt-6" action="#" method="POST">
         <div>
-          <label class="block text-gray-700">Email Address</label>
-          <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required>
+          <label class="block text-gray-700">Email</label>
+          <input type="email" name="" id="" placeholder="Entrer votre Email" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required>
         </div>
 
         <div class="mt-4">
-          <label class="block text-gray-700">Password</label>
-          <input type="password" name="" id="" placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+          <label class="block text-gray-700">Mot de passe</label>
+          <input type="password" name="" id="" placeholder="Entrez votre Mot de passe" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
                 focus:bg-white focus:outline-none" required>
         </div>
 
         <div class="text-right mt-2">
-          <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+          <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Mot de passe oublié?</a>
         </div>
 
         <button type="submit" class="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
-              px-4 py-3 mt-6">Log In</button>
+              px-4 py-3 mt-6">Connexion</button>
       </form>
 
       <hr class="my-6 border-gray-300 w-full">
@@ -42,14 +42,14 @@
             <div class="flex items-center justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
             <span class="ml-4">
-            Log in
-            with
+            Connexion
+            avec
             Google</span>
             </div>
           </button>
 
-      <p class="mt-8">Need an account? <a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Create an
-              account</a></p>
+      <p class="mt-8">Besoin d'un compte? <a href="?page=create-account" class="text-blue-500 hover:text-blue-700 font-semibold">Créez votre
+              compte</a></p>
 
 
     </div>
